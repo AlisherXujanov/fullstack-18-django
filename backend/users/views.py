38eed49c92581fdf8e8420c34475dc0704492df7
Alisher <alisherxujanov163@gmail.com>
2025-08-08 17:54:06 +0500

    model

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
 def home(request):
     context = {
-        "my_name": "Alisher"
+        "my_name": "Alisher",
+        "profile": Profile.objects.first()
     }
     return render(request, 'index.html', context)
