28d91e9ab21fa7d526741554ac52cf34561305d8
Alisher <alisherxujanov163@gmail.com>
2025-08-27 17:22:29 +0500

    Add environment configuration for Django secret key
    - Created a new .env file to store the DJANGO_SECRET_KEY.
    - Updated settings.py to retrieve the secret key from the environment using decouple for improved security.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)vq-!0-ai(!=i-n^h+mu7ux2vn=j8(eka-11z#^@#x*1o2_5lc'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
