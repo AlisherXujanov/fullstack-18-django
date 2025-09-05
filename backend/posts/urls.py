14e3ac9bdb53e11ca29b70d3351108532b43c483
Alisher <alisherxujanov163@gmail.com>
2025-09-05 17:38:07 +0500

    Refactor post management views to use class-based views and add delete confirmation template

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('posts', PostsView.as_view(),  name='get_posts'),
-    path('create_post', create_post,  name='create_post'),
-    path('update_post/<int:pk>', update_post,  name='update_post'),
-    path('delete_post/<int:pk>', delete_post,  name='delete_post'),
+    path('create_post', PostCreateView.as_view(),  name='create_post'),
+    path('update_post/<int:pk>', UpdatePostView.as_view(),  name='update_post'),
+    path('delete_post/<int:pk>', PostDeleteView.as_view(),  name='delete_post'),
 ]
