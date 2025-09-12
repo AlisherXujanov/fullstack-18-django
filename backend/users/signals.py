117979fd71ef5ba03c128313280276bb8815c814
Alisher <alisherxujanov163@gmail.com>
2025-09-12 16:43:45 +0500

    Add user authentication templates and signals
    - Introduced base template for account-related pages.
    - Added login, signup, and logout templates with Bootstrap styling.
    - Implemented user profile creation via signals upon user registration.
    - Updated app configuration to include signal handling.

---
+++ backend/users/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+# post_save  == event in JS
+# receiver   == event listener in JS
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    """This function will be called every time a new user is created."""
+    if created:
+        Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
+
